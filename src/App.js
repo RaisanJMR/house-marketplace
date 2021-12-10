d8103410d14c4ecc615ed5c5b6ac42088079ed1f
RaisanJMR <raisanjmr@gmail.com>
2021-12-10 09:42:58 +0530

    reset password with firebase via email done

--- src/App.js
+++ src/App.js
@@ -6,9 +6,10 @@ import Profile from './pages/Profile'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css';
+import 'react-toastify/dist/ReactToastify.css'
 import ForgotPassword from './pages/ForgotPassword'
 import Navbar from './components/Navbar'
+import { PrivateRoute } from './components/PrivateRoute'
 
 function App() {
   return (
@@ -17,14 +18,16 @@ function App() {
         <Routes>
           <Route path='/' element={<Explore />} />
           <Route path='/offers' element={<Offers />} />
+          <Route path='/profile' element={<PrivateRoute />}>
+            <Route path='/profile' element={<Profile />} />
+          </Route>
           <Route path='/sign-in' element={<SignIn />} />
-          <Route path='/profile' element={<Profile />} />
           <Route path='/sign-up' element={<SignUp />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
         </Routes>
         <Navbar />
       </Router>
-      <ToastContainer/>
+      <ToastContainer />
     </>
   )
 }
